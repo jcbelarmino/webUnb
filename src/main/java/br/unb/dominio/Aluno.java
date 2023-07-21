eb315138db162d73b9681d2c52133639be55558b
jader belarmino <jader.belarmino@objectedge.com>
2023-07-21 10:00:28 -0300

    first commit

---
+++ src/main/java/br/unb/dominio/Aluno.java
@@ -0,0 +1,56 @@
+package br.unb.dominio;
+
+/**
+ * @author 
+ *
+ */
+public class Aluno {
+	private int id;
+	private String nome;
+	private String matricula;
+
+	public Aluno() {
+	}
+
+	public Aluno(String nome, String matricula) {
+		this.nome = nome;
+		this.matricula = matricula;
+	}
+
+	
+	public Aluno(int id, String nome, String matricula) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.matricula = matricula;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	@Override
+	public String toString() {
+		return "Aluno{" + "id=" + id + ", nome='" + nome + '\'' + ", matricula='" + matricula + '\'' + '}';
+	}
+}

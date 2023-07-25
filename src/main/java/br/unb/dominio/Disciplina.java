757e25abf97512ff0894197fd9c0437963cafacd
jader belarmino <jader.belarmino@objectedge.com>
2023-07-25 15:14:53 -0300

    aula 24/07

---
+++ src/main/java/br/unb/dominio/Disciplina.java
@@ -0,0 +1,9 @@
+package br.unb.dominio;
+
+public class Disciplina {
+	private int id; //gerado pelo banco
+	private String nome; // nome da disciplina
+	private String curso; // nome do curso: civil, medicina, direito
+	private String turma; // 20231
+	private String local; // Bloco sala
+}
